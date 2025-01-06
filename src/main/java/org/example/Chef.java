c90cc72a164cfb8ca7a8c4406bb3fe6c146b6f21
plamenRamen <zapparov2010@yandex.ru>
2025-01-06 17:07:12 +0300

    Create branch 'develop', add 'Readme'

--- src/main/java/org/example/Chef.java
+++ src/main/java/org/example/Chef.java
@@ -1,8 +1,22 @@
 package org.example;
 
-public class Chef implements Runnable{
-    @Override
-    public void run() {
+public class Chef implements Runnable {
+    private int id;
+    private int orders = 0;
+    private Restaurant restaurant;
 
+    public Chef(int name, Restaurant restaurant) {
+        this.id = name;
+    }
+
+    public int getOrders() {
+        return this.orders;
+    }
+
+    public void setOrders(int count) {
+        this.orders += count;
+    }
+
+    public void run() {
     }
 }
